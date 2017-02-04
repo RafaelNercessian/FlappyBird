e4d90e66c0a3855e9142454460bc3e7c8bab9517
RafaelNercessian <rafanercessian@gmail.com>
2017-02-04 21:13:05 -0200

    Passaro subindo e descendo

--- core/src/com/android/flappybird/FlappyBird.java
+++ core/src/com/android/flappybird/FlappyBird.java
@@ -5,39 +5,58 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
 
     private SpriteBatch batch;
-    private Texture[] passaro;
+    private Texture[] passaros;
     private Texture fundo;
 
-    private int movimento=0;
     private int larguraDispositivo;
     private int alturaDispositivo;
+    private float variacao=0;
+    private float velocidadeQueda=0;
+    private float posicaoVertical=0;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        passaro=new Texture[3];
-        passaro[0]=new Texture("passaro1.png");
-        passaro[1]=new Texture("passaro2.png");
-        passaro[2]=new Texture("passaro3.png");
+        passaros=new Texture[3];
+        passaros[0]=new Texture("passaro1.png");
+        passaros[1]=new Texture("passaro2.png");
+        passaros[2]=new Texture("passaro3.png");
         fundo=new Texture("fundo.png");
-        
+
 
         larguraDispositivo=Gdx.graphics.getWidth();
         alturaDispositivo=Gdx.graphics.getHeight();
+        posicaoVertical=alturaDispositivo/2;
 
     }
 
     @Override
     public void render() {
-        movimento++;
+        variacao=variacao+Gdx.graphics.getDeltaTime()*10;
+        velocidadeQueda++;
+        if(variacao>2){
+            variacao=0;
+        }
+
+
+        if(Gdx.input.justTouched()){
+            velocidadeQueda=-20;
+        }
+
+
+        if(posicaoVertical>0 || velocidadeQueda<0) {
+            posicaoVertical -= velocidadeQueda/1.25;
+        }
+
         batch.begin();
         batch.draw(fundo,0,0, larguraDispositivo,alturaDispositivo);
-        batch.draw(passaro[0], movimento, alturaDispositivo/2);
+        batch.draw(passaros[(int) variacao], 30, posicaoVertical);
         batch.end();
     }
 
