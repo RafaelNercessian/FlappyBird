79e25f47ae26d8de2b72fafe561aa2b617c2cadf
RafaelNercessian <rafanercessian@gmail.com>
2017-02-04 20:40:48 -0200

    Criando animacao

--- core/src/com/android/flappybird/FlappyBird.java
+++ core/src/com/android/flappybird/FlappyBird.java
@@ -2,32 +2,47 @@ package com.android.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private SpriteBatch batch;
+    private Texture[] passaro;
+    private Texture fundo;
+
+    private int movimento=0;
+    private int larguraDispositivo;
+    private int alturaDispositivo;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        passaro=new Texture[3];
+        passaro[0]=new Texture("passaro1.png");
+        passaro[1]=new Texture("passaro2.png");
+        passaro[2]=new Texture("passaro3.png");
+        fundo=new Texture("fundo.png");
+        
+
+        larguraDispositivo=Gdx.graphics.getWidth();
+        alturaDispositivo=Gdx.graphics.getHeight();
+
+    }
+
+    @Override
+    public void render() {
+        movimento++;
+        batch.begin();
+        batch.draw(fundo,0,0, larguraDispositivo,alturaDispositivo);
+        batch.draw(passaro[0], movimento, alturaDispositivo/2);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
