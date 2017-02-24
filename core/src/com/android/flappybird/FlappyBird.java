c4619674408ae65f5ae97714528cfa94037aad4c
RafaelNercessian <rafanercessian@gmail.com>
2017-02-23 22:17:58 -0300

    App funcionando

--- core/src/com/android/flappybird/FlappyBird.java
+++ core/src/com/android/flappybird/FlappyBird.java
@@ -3,6 +3,8 @@ package com.android.flappybird;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,6 +12,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import java.util.Random;
 
@@ -24,8 +28,8 @@ public class FlappyBird extends ApplicationAdapter {
     private Random numerosRandomico;
     private BitmapFont mensagem;
 
-    private int larguraDispositivo;
-    private int alturaDispositivo;
+    private float larguraDispositivo;
+    private float alturaDispositivo;
     private float variacao=0;
     private float velocidadeQueda=0;
     private float posicaoVertical=0;
@@ -40,6 +44,10 @@ public class FlappyBird extends ApplicationAdapter {
     private Rectangle retanguloCanoTopo;
     private Rectangle retanguloCanoBaixo;
     private ShapeRenderer shape;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private final float VIRTUAL_WIDTH=768;
+    private final float VIRTUAL_HEIGHT=1024;
 
     @Override
     public void create() {
@@ -57,13 +65,17 @@ public class FlappyBird extends ApplicationAdapter {
         passaroCirculo=new Circle();
         mensagem=new BitmapFont();
 
+        camera=new OrthographicCamera();
+        camera.position.set(VIRTUAL_WIDTH/2,VIRTUAL_HEIGHT/2,0);
+        viewport=new StretchViewport(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,camera);
+
         mensagem.setColor(Color.WHITE);
         mensagem.getData().setScale(3);
 
         fonte.setColor(Color.WHITE);
         fonte.getData().setScale(6);
-        larguraDispositivo=Gdx.graphics.getWidth();
-        alturaDispositivo=Gdx.graphics.getHeight();
+        larguraDispositivo=VIRTUAL_WIDTH;
+        alturaDispositivo=VIRTUAL_HEIGHT;
         posicaoVertical=alturaDispositivo/2;
         posicaoMovimentoCanoHorizontal=larguraDispositivo-100;
         espacoCanos=300;
@@ -71,7 +83,16 @@ public class FlappyBird extends ApplicationAdapter {
     }
 
     @Override
+    public void resize(int width, int height) {
+        viewport.update(width,height);
+    }
+
+    @Override
     public void render() {
+
+        camera.update();
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
         variacao = variacao + Gdx.graphics.getDeltaTime() * 10;
         if (variacao > 2) {
@@ -128,6 +149,9 @@ public class FlappyBird extends ApplicationAdapter {
 
 
         }
+
+        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
         batch.draw(fundo,0,0, larguraDispositivo,alturaDispositivo);
         batch.draw(canoAlto,posicaoMovimentoCanoHorizontal,alturaDispositivo/2+espacoCanos/2+alturaEntreCanosRandomica);
